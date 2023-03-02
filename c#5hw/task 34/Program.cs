ed3de19566981a84c2bda3d09443dcf058d344f9
Binasa366 <sabinajamolova2003@gmail.com>
2023-03-02 20:05:03 +0500

    done

---
+++ c#5hw/task 34/Program.cs
@@ -0,0 +1,27 @@
+﻿        void FillArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = new Random().Next(100, 999);
+        }
+
+        int CountNum(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] % 2 == 0)
+                    count++;
+            return count;
+        }
+
+        void PrintArray(int[] array)
+        {
+            foreach (var el in array)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+        }
+
+        int[] array = new int[5];
+        FillArray(array);
+        PrintArray(array);
+        Console.WriteLine($"-> {CountNum(array)}");
+
